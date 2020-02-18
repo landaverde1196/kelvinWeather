b20df5c1693dd3f8bf4a6c33b3013ec423f7c6b9
landaverde1196 <alejndro1196@gmail.com>
2020-02-18 11:39:28 -0600

    first commit

---
+++ kelvinWeather.js
@@ -0,0 +1,15 @@
+const kelvin = 0
+// To start, create a variable named kelvin, and set it equal tok;kjlj 
+const celsius = kelvin - 273;
+// Let’s convert Kelvin to Celsius by subtracting 273 from the 
+let fahrenheit = celsius * (9/5) + 32;
+// Use this equation to calculate Fahrenheit, then store the 
+fahrenheit = Math.floor(fahrenheit);
+// Use the .floor() method from the Math library to round down
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+// convert to newton
+let newton = celsius * (33/100);
+// round down
+newton = Math.floor(newton);
+console.log(`The temperature is ${newton} degrees Newton.`);
+
